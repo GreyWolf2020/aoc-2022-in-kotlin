f4c28e4ee1cb22956f861caa06d2d997477916d3
greyWolf100 <gregphiri95@gmail.com>
2022-12-03 22:56:41 +0200

    The Day02 and Day03 challenges work as expected.

---
+++ src/Day03.kt
@@ -0,0 +1,44 @@
+fun main() {
+    fun itemPriority(c: Char): Int = when  {
+        c.isLowerCase() -> c.code - 96
+        c.isUpperCase() -> c.code - 64 + 26
+        else -> 0
+    }
+    fun part1(input: List<String>): Int = input.foldRight(0) { rucksack, prioritySum ->
+        rucksack
+            .chunked(rucksack.length / 2)
+            .map {
+                it.toSet()
+            }
+            .intersection()
+            .map(::itemPriority)
+            .sum() + prioritySum
+    }
+
+
+
+    fun part2(input: List<String>): Int = input
+            .windowed(3, 3)
+            .foldRight(0) { rucksacks, prioritySum ->
+                rucksacks
+                    .map { it.toSet() }
+                    .intersection()
+                    .map(::itemPriority)
+                    .sum() + prioritySum
+            }
+
+
+    // test if implementation meets criteria from the description, like:
+    val testInput = readInput("Day03_test")
+    check(part1(testInput) == 157)
+
+    check(part2(testInput) == 70)
+
+    val input = readInput("Day03")
+    println(part1(input))
+    println(part2(input))
+}
+
+fun List<Set<Char>>.intersection() = reduce { element, acc ->
+    element.intersect(acc)
+}
